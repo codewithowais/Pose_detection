3f20f084ac035ea50885d5671437242efa5b86c0
Muhammad Owais Ahmed <owaisraza2051@gmail.com>
2022-01-06 23:34:23 +0500

    pose detection and camera and filter on image

---
+++ lib/home.dart
@@ -0,0 +1,47 @@
+import 'package:aiapp/camera.dart';
+import 'package:aiapp/filter.dart';
+import 'package:aiapp/posedetection.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: ElevatedButton(
+          child: const Text("pose"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ChangeNotifierProvider(
+                        create: (_) => PoseDetectionState(),
+                        child: PoseDetectionPage(),
+                      )),
+            );
+          },
+        ),
+        actions: [
+          ElevatedButton(
+            child: const Text("filter"),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Filter_page()),
+              );
+            },
+          )
+        ],
+      ),
+      body: CameraApp(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:aiapp/camera.dart';
+import 'package:aiapp/home.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    logError(e.code, e.description);
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
